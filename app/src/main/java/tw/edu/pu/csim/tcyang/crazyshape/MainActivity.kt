83f796482d455cc2bf9565467198bdcaa2226ef7
tcyang <tcyang@gm.pu.edu.tw>
2021-05-10 17:56:22 +0800

    2021上機考v0_楊子青

---
+++ app/src/main/java/tw/edu/pu/csim/tcyang/crazyshape/MainActivity.kt
@@ -0,0 +1,25 @@
+package tw.edu.pu.csim.tcyang.crazyshape
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Toast.makeText(baseContext, "作者：楊子青", Toast.LENGTH_LONG).show()
+
+        imgNext.setOnLongClickListener(object : View.OnLongClickListener {
+            override fun onLongClick(p0: View?): Boolean {
+                intent = Intent(this@MainActivity, GameActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+        })
+    }
+}
